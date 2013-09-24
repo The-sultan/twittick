171023bf2fa6c3dff4cf118262733f7c0fb8275d
Farid Elias <farid.elias@autodesk.com>
2013-09-23 22:23:53 -0400

    Enhanced box view.

--- resources/partials/dashbox.php
+++ resources/partials/dashbox.php
@@ -1,86 +1,78 @@
 <?php session_start(); ?>
 
-<div class="alert alert-success" style="display: none;">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <span class="tw-alert-message"> </span>
-    <!--<strong>Cool!</strong> The ticket was successfully removed. <a href="#"><i class="icon-undo"></i> Undo</a>.-->
-</div>
-<h1 class="pull-left">Box View</h1>
+<div class="clearfix">
+	<div class="alert alert-success" style="display: none;">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+		<span class="tw-alert-message"> </span>
+		<!--<strong>Cool!</strong> The ticket was successfully removed. <a href="#"><i class="icon-undo"></i> Undo</a>.-->
+	</div>
+	<h1 class="pull-left">Box View</h1>
 
-<div class="btn-group pull-right" data-toggle="buttons-radio">
-    <a href="#dashlist" ng-click="setListView(1)" type="button" class="btn tw-btn-view"><i class="icon-th-large"></i></a>
-    <a href="#dashbox" ng-click="setListView(0)" type="button" class="btn tw-btn-view active"><i class="icon-th-list"></i></a>
+	<div class="btn-group pull-right" data-toggle="buttons-radio">
+		<a href="#dashbox" ng-click="setListView(1)" type="button" class="btn tw-btn-view"><i class="icon-th-list"></i></a>
+		<a href="#dashlist" ng-click="setListView(0)" type="button" class="btn tw-btn-view active"><i class="icon-th-large"></i></a>		
+	</div>
 </div>
-
 <div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Type</th>
-                <th>Status</th>
-                <th>Priority</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr ng-repeat="ticket in tickets | filter:query | orderBy:orderProp" class="tw-{{ticket.priority | lowercase}}">
-                <td><b>{{ticket.title}}</b></td>
-                <td>{{ticket.type}}</td>
-                <td>{{ticket.status}}</td>
-                <td>{{ticket.priority}}</td>
-                <td>
+    
+        <div class="ticket-box-container row-fluid ">
+            <div ng-repeat="ticket in tickets | filter:query | orderBy:orderProp" class="span3 well ">
+                <div><b>{{ticket.title}}</b></div>
+                <div>{{ticket.type}}</div>
+                <div>{{ticket.status}}</div>
+                <div>{{ticket.priority}}</div>
+                <div>
                     <?php if ($_SESSION['user']['role'] == 'requestor') : ?>
 
                         <div ng-switch on="ticket.status">
                             <div ng-switch-when="NEW">
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#/ticket/{{ticket.id}}/edit"><i class="icon-edit"></i> Edit</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="cancelTicket(ticket.id)"><i class="icon-remove"></i> Cancel</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="deleteTicket(ticket.id)"><i class="icon-trash"></i> Delete</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i  class="icon-eye-open"></i></a>&nbsp;&nbsp;&nbsp;
+                                <a href="#/ticket/{{ticket.id}}/edit" bs-tooltip="'Edit'" class="btn btn-info tw-action-btn"><i class="icon-edit"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="cancelTicket(ticket.id)" bs-tooltip="'Cancel'" class="btn btn-info tw-action-btn"><i class="icon-remove"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="deleteTicket(ticket.id)" bs-tooltip="'Delete'" class="btn btn-info tw-action-btn"><i class="icon-trash"></i> </a>
                             </div>
                             <div ng-switch-when="APPROVED">
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" bs-modal="'partials/modal/cancel.php'"><i class="icon-remove"></i> Cancel</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="deleteTicket(ticket.id)"><i class="icon-trash"></i> Delete</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" bs-modal="'partials/modal/cancel.php'" bs-tooltip="'Cancel'" class="btn btn-info tw-action-btn"><i class="icon-remove"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="deleteTicket(ticket.id)" bs-tooltip="'Delete'" class="btn btn-info tw-action-btn"><i class="icon-trash"></i> </a>
                             </div>
                             <div ng-switch-default>
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="deleteTicket(ticket.id)"><i class="icon-trash"></i> Delete</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open" bs-tooltip="'View'" class="btn btn-info tw-action-btn"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="deleteTicket(ticket.id)" bs-tooltip="'Delete'" class="btn btn-info tw-action-btn"><i class="icon-trash"></i> </a>
                             </div>
                         </div>
 
                     <?php elseif ($_SESSION['user']['role'] == 'approver') : ?>
                         <div ng-switch on="ticket.status">
                             <div ng-switch-when="NEW">
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#/ticket/{{ticket.id}}/edit"><i class="icon-edit"></i> Edit</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="approveTicket(ticket.id)"><i class="icon-thumbs-up"></i> Approve</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="rejectTicket(ticket.id)"><i class="icon-thumbs-down"></i> Reject</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="cancelTicket(ticket.id)"><i class="icon-remove"></i> Cancel</a>&nbsp;&nbsp;&nbsp;
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#/ticket/{{ticket.id}}/edit" bs-tooltip="'Edit'" class="btn btn-info tw-action-btn"><i class="icon-edit"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="approveTicket(ticket.id)" bs-tooltip="'Approve'" class="btn btn-info tw-action-btn"><i class="icon-thumbs-up"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="rejectTicket(ticket.id)" bs-tooltip="'Reject'" class="btn btn-info tw-action-btn"><i class="icon-thumbs-down"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="cancelTicket(ticket.id)" bs-tooltip="'Cancel'" class="btn btn-info tw-action-btn"><i class="icon-remove"></i> </a>&nbsp;&nbsp;&nbsp;
                             </div>
                             <div ng-switch-when="APPROVED">
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#/ticket/{{ticket.id}}/edit"><i class="icon-edit"></i> Edit</a>&nbsp;&nbsp;&nbsp;
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#/ticket/{{ticket.id}}/edit" bs-tooltip="'Edit'" class="btn btn-info tw-action-btn"><i class="icon-edit"></i> </a>&nbsp;&nbsp;&nbsp;
                             </div>
                             <div ng-switch-default>
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i> </a>
                             </div>
                         </div>
                     <?php elseif ($_SESSION['user']['role'] == 'executor') : ?>
                         <div ng-switch on="ticket.status">
                             <div ng-switch-when="APPROVED">
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>&nbsp;&nbsp;&nbsp;
-                                <a href="#" ng-click="doneTicket(ticket.id)"><i class="icon-check"></i> Mark as Done</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i> </a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ng-click="doneTicket(ticket.id)" bs-tooltip="'Mark as Done'" class="btn btn-info tw-action-btn"><i class="icon-check"></i></a>
                             </div>
                             <div ng-switch-default>
-                                <a href="#/ticket/{{ticket.id}}"><i class="icon-eye-open"></i> View</a>
+                                <a href="#/ticket/{{ticket.id}}" bs-tooltip="'View'" class="btn btn-info tw-action-btn"><i class="icon-eye-open"></i></a>
                             </div>
                         </div>
                     <?php endif; ?>
-                </td>
-            </tr>
-        </tbody>
+                </div>
+            </div>
+        </div>
     </table>
 </div>
 
